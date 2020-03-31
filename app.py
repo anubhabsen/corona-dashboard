c1e0b349a27b808fe701635a962b340b3f209d69
Anubhab Sen <anubhabsen@gmail.com>
2020-03-31 00:52:47 -0700

    Separate page for states with similar data

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import datetime
+from states import states
 import requests
 app = Flask(__name__)
 
@@ -10,5 +12,27 @@ def index():
 	total_death = str(total['death'])
 	total_hospitalized = str(total['hospitalized'])
 	total_conclusive_tested = str(total['totalTestResults'])
-	updated_time = str(total['lastModified'])
+	updated_time = str(datetime.datetime.strptime(state_info['dateModified'], "%Y-%m-%dT%H:%M:%S%z"))
+	updated_time = updated_time[:-6] + ' UTC'
 	return 'Positives: ' + total_positive + '<br>' + 'Negatives: ' + total_negative + '<br>' + 'Hospitalized: ' + total_hospitalized + '<br>' + 'Tested (with results): ' + total_conclusive_tested + '<br>' + 'Deaths: ' + total_death + '<br>' + 'Last updated time: ' + updated_time
+
+@app.route('/state/<state>')
+def state(state=None):
+	total = requests.get('https://covidtracking.com/api/states').json()
+	state_info = {}
+	for i in total:
+		print(i['state'].lower())
+		if i['state'].lower() == state.lower():
+			state_info = i
+			break
+	if state_info == {}:
+		return 'Enter valid 2 alphabet state abbreviation'
+	state_positive = str(state_info['positive'])
+	state_negative = str(state_info['negative'])
+	state_death = str(state_info['death'])
+	state_pending = str(state_info['pending'])
+	state_hospitalized = str(state_info['hospitalized'])
+	state_conclusive_tested = str(state_info['totalTestResults'])
+	updated_time = str(datetime.datetime.strptime(state_info['dateModified'], "%Y-%m-%dT%H:%M:%S%z"))
+	updated_time = updated_time[:-6] + ' UTC'
+	return '<h1>' + states[state.upper()] + '</h1><br>Positives: ' + state_positive + '<br>' + 'Negatives: ' + state_negative + '<br>' + 'Pending: ' + state_pending + '<br>' + 'Hospitalized: ' + state_hospitalized + '<br>' + 'Tested (with results): ' + state_conclusive_tested + '<br>' + 'Deaths: ' + state_death + '<br>' + 'Last updated time: ' + updated_time
